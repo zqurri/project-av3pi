a163e811e6ee5cbc16e17f0a4b6389e1a05dcb4e
zqurri <dimafrank.28fjx@gmail.com>
2026-06-10 21:40:32 +0200

    route.js

--- app/api /discord / route.js
+++ app/api /discord / route.js
@@ -1,1 +1,15 @@
+export async function POST(req) {
+  const data = await req.json();
 
+  await fetch(process.env.DISCORD_WEBHOOK_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      content: JSON.stringify(data, null, 2)
+    })
+  });
+
+  return Response.json({ ok: true });
+}
